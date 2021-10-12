d4c9f00b2954bc6ee9ffd906c6ea0a8395b5a996
Dave Andrewz <DaveAndrewz@hotmail.com>
2021-10-12 11:11:39 -0500

    Created the number range to be used in JS file

--- guess.js
+++ guess.js
@@ -1,5 +1,7 @@
 // ! GUESS THAT NUMBER GAME ! \\
 
+
+// ! VARIABLES ! \\
 // ? Message to be used throughout the file/project ? \\
 const enterNumText = `Please enter a number greater than zero`;
 // ? For restarting the game ? \\
@@ -15,4 +17,31 @@ let guess;
 // ? For storing users response to play again or not play again ? \\
 let playAgain;
 
+
+
+// ! GAME MEAT ! \\
+
+
+// ? STARTING ALERT MESSAGE TO SAY WELCOME TO GAME ? \\
+//
+alert(`Welcome to "GUESS THAT NUMBER!!" 
+Please click "OK" to start the game.`);
+
+// ? Game restarts as long as restartGame has a value of TRUE ? \\
+//
+while (restartGame){
+    //  Asks user to enter number to set the upper bound for the random number that will be created (AKA Number to be guessed).
+    rangeNum = prompt(`Please enter a maximum number for the range:`);
+    //  Using parseInt to attempt to convert the users response to a number value. (NOTE: The value returned from a prompt is ALWAYS a string value). Also, if the value cannot be converted then the value returned will be NaN (Not A Number)
+    rangeNum = parseInt(rangeNum); // tries to convert prompt entry into a number value.
+
+    // 
+    while (!rangeNum || rangeNum < 1){
+        rangeNum = prompt(enterNumText);
+        rangeNum = parseInt(rangeNum);
+    }
+
+
+    break; // infinite loop safeguard
+}
 
