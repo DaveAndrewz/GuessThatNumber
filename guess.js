cd047ada8accc2263e59c89a6d3007e91e51e0a9
Dave Andrewz <DaveAndrewz@hotmail.com>
2021-10-12 10:15:48 -0500

    Created the variables in JS file for the game

---
+++ guess.js
@@ -0,0 +1,18 @@
+// ! GUESS THAT NUMBER GAME ! \\
+
+// ? Message to be used throughout the file/project ? \\
+const enterNumText = `Please enter a number greater than zero`;
+// ? For restarting the game ? \\
+let restartGame = true;
+// ? For storing the range of the random number that is going to be created ? \\
+let rangeNum;
+// ? For storing the number to be guessed ? \\
+let randomNum;
+// ? For storing the number of attempts that the user has left ? \\
+let attempts;
+// ? For storing the users guess ? \\
+let guess;
+// ? For storing users response to play again or not play again ? \\
+let playAgain;
+
+
